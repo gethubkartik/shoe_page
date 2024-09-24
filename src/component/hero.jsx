3ea39939d1e564534cedde817295e25ccedef518
gethubkartik <Gupta1234@#>
2024-09-24 13:55:51 +0530

    Initial commit

---
+++ src/component/hero.jsx
@@ -0,0 +1,34 @@
+const HeroSection = () => {
+    return (
+
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <div className="btn">
+                    <button>Shop Now</button>
+                    <button className="btn2"> Category</button>
+                </div>
+                <div className="shopping">Also Available On
+
+                    <div className="img-icon">
+                        <img src="public/img/shops.png" alt="shop logo" />
+                    </div>
+                </div>
+
+            </div>
+            <div className="hero-img">
+                <img src="public/img/shoe_image.png" alt="image not found" width={520} height={467}/>
+            </div>
+
+
+        </main>
+
+
+
+
+    )
+};
+
+
+export default HeroSection;
---
+++ src/component/navigation.jsx
@@ -0,0 +1,25 @@
+const Navigation = () => {
+    return (
+
+        <nav className="container">
+            <div className='logo'>
+                <img src="public/img/nike1.jpg" alt="images not found " width={90} height={50} />
+            </div>
+            <div>
+                <ul>
+
+                    <li>Menu</li>
+                    <li>Location</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='button'>
+                <button>Login</button>
+            </div>
+        </nav>
+
+
+    );
+};
+export default Navigation;
